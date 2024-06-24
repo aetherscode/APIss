bf3f02e552ccbdb97f2c0825a5819425ce5c5bf0
Shannz <134748195+Shannzx10@users.noreply.github.com>
2024-06-24 07:50:21 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { thinkany, GoodyAI, luminai, blackbox } = require('./scrape/scraper');
+const { thinkany, GoodyAI, luminai, blackbox, CgtAi, Simsimi, leptonAi, yousearch, LetmeGpt, AoyoAi } = require('./scrape/scraper');
 const config = require('./config');
 const msg = config.messages;
 const app = express();
@@ -75,6 +75,120 @@ app.get('/ai/blackbox', async (req, res) => {
   }
   try {
     const result = await blackbox(query);
+    const response = {
+      status: true,
+      code: 200,
+      author: config.author,
+      result: result
+    };
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ status: false, code: 500, author: config.author, result: msg.error });
+  }
+});
+
+app.get('/ai/cgt', async (req, res) => {
+  const query = req.query.query;
+  if (!query) {
+    return res.status(400).json({ status: false, code: 400, author: config.author, result: msg.query });
+  }
+  try {
+    const result = await CgtAi(query);
+    const response = {
+      status: true,
+      code: 200,
+      author: config.author,
+      result: result
+    };
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ status: false, code: 500, author: config.author, result: msg.error });
+  }
+});
+
+app.get('/ai/simsimi', async (req, res) => {
+  const query = req.query.query;
+  if (!query) {
+    return res.status(400).json({ status: false, code: 400, author: config.author, result: msg.query });
+  }
+  try {
+    const result = await Simsimi(query);
+    const response = {
+      status: true,
+      code: 200,
+      author: config.author,
+      result: result
+    };
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ status: false, code: 500, author: config.author, result: msg.error });
+  }
+});
+
+app.get('/ai/lepton', async (req, res) => {
+  const query = req.query.query;
+  if (!query) {
+    return res.status(400).json({ status: false, code: 400, author: config.author, result: msg.query });
+  }
+  try {
+    const result = await leptonAi(query);
+    const response = {
+      status: true,
+      code: 200,
+      author: config.author,
+      result: result
+    };
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ status: false, code: 500, author: config.author, result: msg.error });
+  }
+});
+
+app.get('/ai/yousearch', async (req, res) => {
+  const query = req.query.query;
+  if (!query) {
+    return res.status(400).json({ status: false, code: 400, author: config.author, result: msg.query });
+  }
+  try {
+    const result = await yousearch(query);
+    const response = {
+      status: true,
+      code: 200,
+      author: config.author,
+      result: result
+    };
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ status: false, code: 500, author: config.author, result: msg.error });
+  }
+});
+
+app.get('/ai/letmegpt', async (req, res) => {
+  const query = req.query.query;
+  if (!query) {
+    return res.status(400).json({ status: false, code: 400, author: config.author, result: msg.query });
+  }
+  try {
+    const result = await LetmeGpt(query);
+    const response = {
+      status: true,
+      code: 200,
+      author: config.author,
+      result: result
+    };
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ status: false, code: 500, author: config.author, result: msg.error });
+  }
+});
+
+app.get('/ai/aoyo', async (req, res) => {
+  const query = req.query.query;
+  if (!query) {
+    return res.status(400).json({ status: false, code: 400, author: config.author, result: msg.query });
+  }
+  try {
+    const result = await AoyoAi(query);
     const response = {
       status: true,
       code: 200,
